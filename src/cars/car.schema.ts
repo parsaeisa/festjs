217b046ee81aa5e0507d500d4c83fb27c68ad19d
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-21 16:59:00 +0200

    add relation for car

--- src/cars/car.schema.ts
+++ src/cars/car.schema.ts
@@ -1,5 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 import { HydratedDocument } from 'mongoose';
+import { User } from 'src/users/user.schema';
 
 export type CatDocument = HydratedDocument<Car>;
 
@@ -13,6 +15,9 @@ export class Car {
 
   @Prop()
   color: string;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'Owner' })
+  owner: User;
 }
 
 export const CatSchema = SchemaFactory.createForClass(Car);
