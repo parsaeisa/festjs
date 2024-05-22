a946569b6a6f15dee5c8513bf1c22dcebabd902e
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-22 11:00:41 +0200

    create prisma connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PostsModule } from './posts/posts.module';
+import { DbModule } from './db/db.module';
+import { PostModule } from './post/post.module';
+import { AuthorModule } from './author/author.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest'),
+    PostsModule,
+    DbModule,
+    PostModule,
+    AuthorModule // TODO: What to put here ???
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
