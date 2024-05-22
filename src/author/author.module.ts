b05664ab454c6074d7d3cee38f5aa628e1547934
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-22 12:01:44 +0200

    import db module

--- src/author/author.module.ts
+++ src/author/author.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthorService } from './author.service';
 import { AuthorController } from './author.controller';
+import { DbModule } from 'src/db/db.module';
 
 @Module({
   controllers: [AuthorController],
   providers: [AuthorService],
+  imports: [DbModule],
 })
 export class AuthorModule {}
