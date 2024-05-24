dec91f0bd49ec0135ca547a3629531b34a479686
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-24 12:35:13 +0200

    add get, set and resset methods

--- src/cache/cache.service.ts
+++ src/cache/cache.service.ts
@@ -1,4 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { Cache } from 'cache-manager';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
-export class CacheService {}
+export class CacheService {
+    constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+    async getRecord(
+        key: string
+    ) {
+        const value = await this.cacheManager.get(key);
+    }
+
+    async setRecord(
+        key: string,
+        value: string,
+    ) {
+        await this.cacheManager.set(key, value);
+    }
+
+    async reseet() {
+        await this.cacheManager.reset();
+    }
+}
