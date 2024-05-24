9f55eb8d8e034ead8ddd7722db478592347b77bc
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-24 11:39:59 +0200

    generate module and service for cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { DbModule } from './db/db.module';
 import { PostModule } from './post/post.module';
 import { AuthorModule } from './author/author.module';
+import { CacheModule } from './cache/cache.module';
 
 @Module({
   imports: [
     DbModule,
     PostModule,
-    AuthorModule // TODO: What to put here ???
+    AuthorModule,
+    CacheModule // TODO: What to put here ???
   ],
   controllers: [AppController],
   providers: [AppService],
