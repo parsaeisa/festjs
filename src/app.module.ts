1d12cd51984317747232f6a8c6dadb43f5cf7c49
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-27 15:30:56 +0200

    add config to app modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { DbModule } from './db/db.module';
 import { PostModule } from './post/post.module';
 import { AuthorModule } from './author/author.module';
 import { CacheModule } from './cache/cache.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -13,7 +14,10 @@ import { CacheModule } from './cache/cache.module';
     PostModule,
     AuthorModule,
     CacheModule,
-  ],
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),    
+  ],  
   controllers: [AppController],
   providers: [AppService],
 })
