d6ddff321b6a034c120f5e65ab2cbeabc7bfb3e9
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-27 15:46:25 +0200

    remove mongoos from app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
 import { DbModule } from './db/db.module';
 import { PostModule } from './post/post.module';
 import { AuthorModule } from './author/author.module';
