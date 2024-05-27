4967050ee03c49de2791117365c4b6a4e459a389
parsa.eissazade <Parsa.eissazadeh@snapp.cab>
2024-05-27 12:02:55 +0200

    insert nest cache manager and controllers to cache module

--- src/cache/cache.module.ts
+++ src/cache/cache.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
 import { CacheService } from './cache.service';
+import { CacheModule as CM } from '@nestjs/cache-manager';
+import { CacheController } from './cache.controller';
 
 @Module({
-  providers: [CacheService]
+  providers: [CacheService],
+  imports: [
+    CM.register({
+      ttl: 5,
+      max: 10,
+    })
+  ],
+  controllers: [CacheController]
 })
 export class CacheModule {}
